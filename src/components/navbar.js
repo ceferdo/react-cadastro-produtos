8d7ecfe0c598f40c7beed8c3ab76330f747a93e8
Celso Fernando Paes <ceferdo@gmail.com>
2021-04-13 17:10:21 -0300

    finalizacao do CRUD

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import App from '../App';
+import { Link } from 'react-router-dom';
 
 function Navbar(){
     return(
@@ -18,15 +19,15 @@ function Navbar(){
             <div className="collapse navbar-collapse" id="navbarColor01">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Home
+                        <Link className="nav-link" to="/">Home
                         <span className="sr-only">(current)</span>
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Cadastro</a>
+                        <Link className="nav-link" to="/cadastro-produtos">Cadastro</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Consulta</a>
+                        <Link className="nav-link" to="/consulta-produtos">Consulta</Link>
                     </li>
                 </ul>
             </div>
--- src/views/produtos/cadastro.js
+++ src/views/produtos/cadastro.js
@@ -1,25 +1,118 @@
-import React from 'react';
+import React from 'react'
+import ProdutoService from '../../app/produtoService'
+import { withRouter } from 'react-router-dom'
+
+const estadoInicial = {
+    nome: '',
+    sku: '',
+    descricao: '',
+    preco: 0,
+    fornecedor:'',
+    sucesso: false,
+    errors: [],
+    atualizando: false
+}
 
 class CadastroProduto extends React.Component {
+
+    state = estadoInicial;
+
+    constructor(){
+        super()
+        this.service = new  ProdutoService();
+    }
+
+    onChange = (event) => {
+        const valor = event.target.value
+        const nomeDoCampo = event.target.name
+        this.setState({ [nomeDoCampo]: valor})
+
+    }
+
+    onSubmit = (event) => {
+        const produto = {
+            nome: this.state.nome,
+            sku: this.state.sku,
+            descricao: this.state.descricao,
+            preco: this.state.preco,
+            fornecedor: this.state.fornecedor
+        }
+        try{
+            this.service.salvar(produto)
+            this.limpaCampos()
+            this.setState({ sucesso: true})
+        }catch(erro){
+            const errors = erro.errors
+            this.setState({errors : errors})
+        }
+        
+    }
+
+    limpaCampos = () => {
+        this.setState(estadoInicial)
+    }
+
+    componentDidMount() {
+        const sku = this.props.match.params.sku
+
+        if (sku)
+        {
+           const resultado =  this.service.obterProdutos().filter( produto => produto.sku === sku )
+           if (resultado.length === 1)
+           {
+               const produtoEncontrado = resultado[0]
+               this.setState({...produtoEncontrado, atualizando: true })
+           }
+        }
+    }
+
     render(){
         return(
             <div className="card">
                 <div className="card-header">
-                    Cadastro de Produto
+                    { this.state.atualizando ? 'Atualização ' : 'Cadastro '}
+                    de Produto
                 </div>
                 <div className="card-body">
-                    
+                
+                { this.state.sucesso &&
+                    <div class="alert alert-dismissible alert-success">
+                        <button type="button" class="close" data-dismiss="alert">&times;</button>
+                        <strong>Bem feito!</strong> Cadastro realizado com sucesso! .
+                    </div>
+                }
+
+                { this.state.errors.length > 0 &&
+                    this.state.errors.map(msg => {
+                        return(
+                            <div class="alert alert-dismissible alert-danger">
+                                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                                <strong>Erro!</strong> { msg }
+                            </div>
+                        )
+                    })
+                }
+
                     <div className="row">
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Nome: *</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" 
+                                       name="nome"
+                                       onChange={this.onChange} 
+                                       value={this.state.nome} 
+                                       className="form-control" />
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>SKU: *</label>
-                                <input type="text" className="form-control" />
+                                <input type="text"
+                                       name="sku"
+                                       disabled={this.state.atualizando}
+                                       onChange={this.onChange} 
+                                       value={this.state.sku}
+                                       className="form-control" />
                             </div>
                         </div>
                     </div>
@@ -28,7 +121,10 @@ class CadastroProduto extends React.Component {
                         <div className="col-md-12">
                             <div className="form-group">
                                 <label>Descrição:</label>
-                                <textarea className="form-control"></textarea>
+                                <textarea name="descricao"
+                                          onChange={this.onChange} 
+                                          value={this.state.descricao}
+                                          className="form-control"></textarea>
                             </div>
                         </div>
                     </div>
@@ -37,7 +133,11 @@ class CadastroProduto extends React.Component {
                         <div className="col-md-12">
                             <div className="form-group">
                                 <label>Preço: *</label>
-                                <input type="text" className="form-control" />
+                                <input type="text"
+                                       name="preco"
+                                       onChange={this.onChange} 
+                                       value={this.state.preco}
+                                       className="form-control" />
                             </div>
                         </div>
                     </div>
@@ -46,17 +146,23 @@ class CadastroProduto extends React.Component {
                         <div className="col-md-12">
                             <div className="form-group">
                                 <label>Fornecedor: *</label>
-                                <input type="text" className="form-control" />
+                                <input type="text"
+                                       name="fornecedor"
+                                       onChange={this.onChange} 
+                                       value={this.state.fornecedor}
+                                       className="form-control" />
                             </div>
                         </div>
                     </div>
 
                     <div className="row">
                         <div className="col-md-1">
-                            <button className="btn btn-success">Salvar</button>
+                            <button onClick={this.onSubmit} className="btn btn-success">
+                                {this.state.atualizando ? 'Atualizar' : 'Salvar'}
+                            </button>
                         </div>
                         <div className="col-md-1">
-                            <button className="btn btn-primary">Limpar</button>
+                            <button onClick={this.limpaCampos} className="btn btn-primary">Limpar</button>
                         </div>
                     </div>
                 </div>
@@ -66,4 +172,4 @@ class CadastroProduto extends React.Component {
 }
 
 
-export default CadastroProduto;
+export default withRouter(CadastroProduto);
